79eb592e1024d4f15ab42a407be7da6ca128fe58
Nelson Vilte <nelsonvilte@gmail.com>
2022-04-29 01:34:41 -0300

    modificado componentes, estilos responsive y logo, agregado nuevo boton copiar

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */
-const btnEncripter = document.querySelector(".btn-encripter")
+const btnEncripter = document.querySelector("#btn-encripter")
 const btnDecryptor = document.querySelector("#decripter");
+
+const btnCopy = document.querySelector("#btn-copy");
+const textOut = document.querySelector("#text-out");
+
+btnCopy.style.display="none";
+textOut.style.display="none";
 
 btnEncripter.addEventListener("click",function(e){
     e.preventDefault();
@@ -17,11 +23,15 @@ btnEncripter.addEventListener("click",function(e){
     let texto=textoIn.value; 
     let msjeUno=document.getElementById("texto-uno");
     let msjeDos=document.getElementById("texto-dos");
-
     let textCod = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g,"ufat" );
+    const imgEncoder = document.querySelector("#img-encoder-one");
     msjeUno.style.display="none";
-    msjeDos.textContent = textCod;  
+    msjeDos.style.display="none";
+    textOut.style.display="";
+    textOut.value = textCod; 
+    imgEncoder.style.display="none";
     textoIn.value = "";
+    btnCopy.style.display="";
 
 
 
@@ -32,15 +42,15 @@ btnEncripter.addEventListener("click",function(e){
 btnDecryptor.addEventListener("click",function(e){
     e.preventDefault();
     decodificar();
-    document.getElementById("texto").value="";
+    document.getElementById("texto-in").value="";
 });
 
 
 
 function decodificar(){
-    let textoIngresado= document.getElementById("texto").value;
+    let textoIngresado= document.getElementById("texto-in").value;
     let decodificado = textoIngresado.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g,"u" ); 
-    document.getElementById("texto-dos").textContent = decodificado;
+    document.getElementById("text-out").value = decodificado;
     
     
 }
