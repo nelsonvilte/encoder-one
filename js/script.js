f981d67579932dc5005abdedb5804ac18a4164c3
Nelson Vilte <nelsonvilte@gmail.com>
2022-05-01 01:19:32 -0300

    modificados estilos y comportamiento componente imagen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /*
 "llaves" de encriptación:
-
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "a" es convertida para "ai"
@@ -12,7 +11,7 @@ const btnDecryptor = document.querySelector("#decripter");
 
 const btnCopy = document.querySelector("#btn-copy");
 const textOut = document.querySelector("#text-out");
-
+const imgEncoder = document.querySelector("#img-encoder-one");
 btnCopy.style.display="none";
 textOut.style.display="none";
 
@@ -23,24 +22,32 @@ btnEncripter.addEventListener("click",function(e){
     let texto=textoIn.value; 
     let msjeUno=document.getElementById("texto-uno");
     let msjeDos=document.getElementById("texto-dos");
-    let imgContent = document.querySelector(".img-content");
+    let errors = validarTexto(texto);
+    imgEncoder.style.display="";
+    if(errors.length > 0){     
+        msjeDos.style.display="none";
+        msjeUno.style.display="none";
+        textOut.style.display="none";
+        btnCopy.style.display="none";
+        mostrarErrores(errors);
+        return;
+    }
+    else{
     let textCod = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g,"ufat" );
-    const imgEncoder = document.querySelector("#img-encoder-one");
-    imgContent.style.paddingTop="10px";
     msjeUno.style.display="none";
+    msjeDos.style.display="revert";
     msjeDos.textContent="Texto procesado"
     textOut.style.display="";
-    // textOut.style.height="400px";
     textOut.value = textCod; 
     imgEncoder.style.display="none";
     textoIn.value = "";
+    let mensajesErrores = document.querySelector("#msjs-errores");
+    mensajesErrores.innerHTML="";
     btnCopy.style.display="";
-
-
-
     
-});
+    }
 
+});
 
 btnDecryptor.addEventListener("click",function(e){
     e.preventDefault();
@@ -48,12 +55,36 @@ btnDecryptor.addEventListener("click",function(e){
     document.getElementById("texto-in").value="";
 });
 
-
-
 function decodificar(){
     let textoIngresado= document.getElementById("texto-in").value;
     let decodificado = textoIngresado.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g,"u" ); 
     document.getElementById("text-out").value = decodificado;
-    
+   
+}
+
+function capturarDatosTexto(textArea){
+    let texto=document.getElementById(textArea);
+    return texto.value
+}
+
+function validarTexto(textos){
+    let errores=[];
+    if(textos.length == 0){
+        errores.push("Ningún mensaje fue encontrado");
+        errores.push("Ingresa el texto que desees encriptar o desencriptar");
+    }
+    return errores; 
+}
+
+function mostrarErrores(errores){
+    let ul = document.querySelector("#msjs-errores");
+    ul.innerHTML = ""
+    errores.forEach(function(error){
+        let li = document.createElement("li");
+        li.textContent = error;
+        li.classList.add("msje1");
+        ul.appendChild(li);
+    });
     
 }
+
